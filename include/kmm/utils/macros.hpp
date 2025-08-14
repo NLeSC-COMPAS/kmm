57c205fa20a83186c93a158ef952506a56ee3288
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2025-08-14 13:48:21 +0200

    Revert a change that caused the HIP backend to fail compilation.

--- include/kmm/utils/macros.hpp
+++ include/kmm/utils/macros.hpp
@@ -27,7 +27,7 @@
     // HIP
     // TODO: using __forceinline__ breaks compilation, it needs to be investigated
     #define KMM_HOST_DEVICE          __host__ __device__
-    #define KMM_DEVICE               __device__ __forceinline__
+    #define KMM_DEVICE               __device__
     #define KMM_HOST_DEVICE_NOINLINE __host__ __device__
     #define KMM_DEVICE_NOINLINE      __device__
     #ifdef __HIP_DEVICE_COMPILE__
